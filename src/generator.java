417effb43fbef439ad5318fce3b913832bf9fe5f
mcdylanb <dylansatgalab@gmail.com>
2019-12-04 16:41:02 +0800

    Big Ui Update: +ask for user input on how many LLA and how many the user wants to generate +add sepeartions for ease of view + added some indention and ln after each generated serial +added a variable in which adds additional letter at the end of every serial to create many versions
    - ask where to start

--- src/generator.java
+++ src/generator.java
@@ -2,6 +2,7 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class generator {
     public static void main(String[] args) {
@@ -11,18 +12,79 @@ public class generator {
         DateFormat dfsa = new SimpleDateFormat("ddMMyyyy");
         DateFormat dfsb = new SimpleDateFormat("ddMMyHHmmaa");
         Date dateobj = new Date();
-        //RepairOrderNumber
-        System.out.println("RO_CS" + dfsa.format(dateobj));
-        //Serial Part HLA
-        System.out.println("SERPART" + dfsa.format(dateobj));
-        //Serial System
-        System.out.println("SERSYS" + dfsa.format(dateobj));
-        //Serial Part LLA
-        System.out.println("SERPAR" + dfsa.format(dateobj));
-        System.out.println(dfsb.format(dateobj));
 
-        Calendar calobj = Calendar.getInstance();
-        System.out.println(calobj.getTime());
+
+
+        //TODO:
+        // - also how to input lla and hla
+        // - ask what letter to start
+
+        Scanner scan = new Scanner(System.in);
+
+        //ask how many serial numbers does the user want to generate
+        System.out.println("Number of Serial to generate:");
+        int generates = scan.nextInt();
+
+
+        // FIXME:
+        //  - if the number of generated serial doesnt have the same number of LLA
+        //  - we can ask for each individual generated chuchu
+        System.out.println("Number of LLA you are using:");
+        int number_of_lla = scan.nextInt();
+
+        //letter to start
+        char alphabet = 'A';
+
+
+        // FIXME:
+        //  -change the int name of x and y
+
+        //creates serial base on number requested by the user
+        for (int y=1; y <= generates; y++){
+            System.out.println("=========="+ y + "/" + generates + "==========");
+            //RepairOrderNumber
+            System.out.println("Repair Order Number:");
+            System.out.println("    RO_CS" + dfsa.format(dateobj)+alphabet);
+            System.out.println("");
+
+            //Serial Part HLA
+            System.out.println("HLA Serial:");
+            System.out.println("    SERPART" + dfsa.format(dateobj)+alphabet);
+            System.out.println("");
+
+            //Serial System
+            System.out.println("Serial System:");
+            System.out.println("    SERSYS" + dfsa.format(dateobj)+alphabet);
+            System.out.println("");
+
+            System.out.println("LLA Serial:");
+            char alphabetlla = 'A';
+            for (int x=0; x < number_of_lla; x++){
+                //Serial Part LLA
+                System.out.println("    SERPAR" + dfsa.format(dateobj)+alphabet+alphabetlla);
+                alphabetlla++;
+            }
+            System.out.println("");
+
+
+            //Customer ID
+            System.out.println("Customer ID:");
+            System.out.println("    "+dfsb.format(dateobj));
+            System.out.println("");
+
+            //changes the letter after every generated serial
+            alphabet++;
+        }
+
+
+
+
+//        for (char alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
+//
+//        }
+
+
+
 
 //        https://beginnersbook.com/2013/05/current-date-time-in-java/
     }
